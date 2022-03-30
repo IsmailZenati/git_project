244dc2a05f3de784b348cb147e1aa041497e5049
IsmailZenati <101001110+IsmailZenati@users.noreply.github.com>
2022-03-30 10:27:54 +0100

    Add files via upload

---
+++ enigme du projet/enigme.h
@@ -0,0 +1,12 @@
+#ifndef QUIZ_H_
+#define QUIZ_H_
+
+
+
+void enigme(SDL_Surface *ecran,int d);
+void reponse(SDL_Surface *ecran,int d);
+void solution(SDL_Surface *ecran,int d);
+void correct(SDL_Surface *ecran);
+
+
+#endif
